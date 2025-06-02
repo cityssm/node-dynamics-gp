c723689dd8975ad7eedc8a03f21d34e6ea63d994
Dan Gowans <dangowans@users.noreply.github.com>
2025-06-02 11:39:34 -0400

    update linter

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,7 +3,8 @@ import eslintConfigCityssm, {
   tseslint
 } from 'eslint-config-cityssm'
 
-const config = tseslint.config(...eslintConfigCityssm, {
+const config = tseslint.config(eslintConfigCityssm, {
+  files: ['**/*.ts'],
   rules: {
     '@cspell/spellchecker': 'off'
   }
